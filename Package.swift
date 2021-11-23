59a240fc9dc34131c56a52ccaff19cd16819908c
WANG Jie <2981971+wangjiejacques@users.noreply.github.com>
2021-11-23 16:01:12 +0100

    add more dependency

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "DynamicQ",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,6 +15,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.0.0"),
+        .package(url: "https://github.com/adam-fowler/mqtt-nio.git", from: "2.1.0"),
+        .package(url: "https://github.com/soto-project/soto-core.git", from: "5.5.0")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
@@ -24,7 +27,11 @@ let package = Package(
             name: "DynamicQ",
             dependencies: [
                 .product(name: "GRPC", package: "grpc-swift"),
-            ]),
+                .product(name: "MQTTNIO", package: "mqtt-nio"),
+                .product(name: "SotoSignerV4", package: "soto-core")
+            ],
+            path: "Sources"
+        ),
         .testTarget(
             name: "DynamicQTests",
             dependencies: ["DynamicQ"]),
